5c92b41af2f3d34a4e401b44d86e3e3ea25a2bd7
xsonali <villshona@gmail.com>
2025-08-07 17:12:04 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,8 +7,6 @@ variable "admin_password" {
   description = "Password for VM"
   type        = string
   sensitive   = true
-  default     = null
-  nullable    = true
 }
 
 variable "vmsize" {
@@ -24,7 +22,6 @@ variable "p2s_root_cert" {
 variable "ssh_public_key" {
   description = "SSH public key for VM access"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPP9NI83LHtJgvN4RMsYP4eHCKM3BehbSM04n1nbL6XrUVnXFau4KxSza+IKQmZs9kv0ktrVhbEZAfEyB87qT85w6ZjGb0Nmcm07YtXE2fUY8sBnxw1KbQ2tIJC+lz1NZdEnqJU9OgoRnLqzH8Xo44BKQYAvynGq/Ah/rFkp9QtRMSFFeG/peYHLe6LqltRXxjeQLrQPydx9EaWRvwuCvnGGgCvt/EYarJym1A5KtQjvSixl7hYS21CveCdm8OvC7YvkeTvvQdYzkFNki+hbpIfCRvBW+GUuLl5gb1C+2+I0aliv/krEdYqBz8Vx2OqI7TERTaL0r8gaM6MorV4cKL alam@Liqustra-2"
 }
 
 variable "os_type" {
