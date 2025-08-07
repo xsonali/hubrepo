5bc62ca19b51cb384d638c2dd6c2fb12d3754363
xsonali <villshona@gmail.com>
2025-08-07 15:29:09 +1000

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# backend.tf (DO NOT pust this file to GitHub)
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "backend-rg"
-    storage_account_name = "backendstorage747"
-    container_name       = "ptstate"
-    key                  = "tfstate"
-  }
-}
