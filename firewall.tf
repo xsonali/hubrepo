74b91440f892727f7eb4c4b7f2e5e1f4d59adced
Gholam Alam <luqustra@gmail.com>
2025-08-09 12:14:57 +1000

    Add Azure Firewall RDP network and NAT rules

--- firewall.tf
+++ firewall.tf
@@ -74,3 +74,30 @@ resource "azurerm_firewall_policy_rule_collection_group" "rdp_rule_group" {
   }
 }
 
+resource "azurerm_firewall_policy_rule_collection_group" "rdp_network_rule_group" {
+  name               = "RDPNetworkRuleCollectionGroup"
+  firewall_policy_id = azurerm_firewall_policy.fw_policy.id
+  priority           = 200
+
+  network_rule_collection {
+    name     = "AllowRDPInboundOutbound"
+    priority = 100
+    action   = "Allow"
+
+    rule {
+      name                  = "Allow-RDP-Inbound"
+      protocols             = ["TCP"]
+      source_addresses      = ["192.168.1.0/24"] # Your client IP range or 0.0.0.0/0 to allow all
+      destination_addresses = [azurerm_public_ip.firewall_pip1.ip_address]
+      destination_ports     = ["3389"]
+    }
+
+    rule {
+      name                  = "Allow-RDP-Outbound"
+      protocols             = ["TCP"]
+      source_addresses      = ["10.0.1.4"] # Firewall private IP
+      destination_addresses = ["10.0.3.4"] # VM private IP
+      destination_ports     = ["3389"]
+    }
+  }
+}
