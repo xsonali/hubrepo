6299c41b6939311b321f014676563892a256e864
Gholam Alam <luqustra@gmail.com>
2025-08-09 20:50:35 +1000

    Add/update privateDNSzone.tf with private DNS zones configuration

---
+++ privateDNSzone.tf
@@ -0,0 +1,11 @@
+# Private DNS Zone - Blob Storage
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.hub_vnet_rg.name
+}
+
+# Private DNS Zone - Internal Corp
+resource "azurerm_private_dns_zone" "custom_dns_link" {
+  name                = "internal.corp"
+  resource_group_name = azurerm_resource_group.hub_vnet_rg.name
+}
