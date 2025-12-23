3632e718654e4427b31ad6971552c759170500ea
xsonali <villshona@gmail.com>
2025-12-23 21:15:45 +1100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
   resource_group_name = "backend-rg"
-  storage_account_name = "backendstorge747x"
+  storage_account_name = "backendstorge747"
   container_name       = "ptstate"
   key                  = "tfstate"
   }
